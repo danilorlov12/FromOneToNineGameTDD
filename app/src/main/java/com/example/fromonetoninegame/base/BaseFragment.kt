39ceb53c3e87406fe42b7d131f741ece58e04160
danilorlov12 <danil.orlov.3022013@gmail.com>
2022-11-29 17:33:25 +0200

    implemented menu fragment and navigation

--- app/src/main/java/com/example/fromonetoninegame/base/BaseFragment.kt
+++ app/src/main/java/com/example/fromonetoninegame/base/BaseFragment.kt
@@ -6,11 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
 
 abstract class BaseFragment<VM: BaseViewModel> : Fragment() {
 
     protected open lateinit var viewModel: VM
     protected abstract val layoutId: Int
+    protected lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,4 +25,9 @@ abstract class BaseFragment<VM: BaseViewModel> : Fragment() {
     }
 
     protected abstract fun viewModelClass(): Class<VM>
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = findNavController()
+    }
 }
---
+++ app/src/main/java/com/example/fromonetoninegame/presentation/menu/MenuViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.fromonetoninegame.presentation.menu
+
+import android.app.Application
+import com.example.fromonetoninegame.base.BaseViewModel
+
+class MenuViewModel(app: Application) : BaseViewModel(app) {
+
+    fun hasStoredGame() = false
+}
